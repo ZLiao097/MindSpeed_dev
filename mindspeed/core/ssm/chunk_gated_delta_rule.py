41c4047ac91db5bd4e13eed862f50e3443d55b0e
wuweiqiang24 <wuweiqiang11@huawei.com>
2026-03-30 20:15:29 +0800

    clean code

--- mindspeed/core/ssm/chunk_gated_delta_rule.py
+++ mindspeed/core/ssm/chunk_gated_delta_rule.py
@@ -1,3 +1,4 @@
+# Copyright (c) 2024, NVIDIA CORPORATION. All rights reserved.
 # Copyright (c) 2025, HUAWEI CORPORATION.  All rights reserved.
 
 import warnings
--- mindspeed/features_manager/context_parallel/context_parallel_feature.py
+++ mindspeed/features_manager/context_parallel/context_parallel_feature.py
@@ -128,6 +128,6 @@ class ContextParallelFeature(MindSpeedFeature):
                 'megatron.core.models.common.embeddings.rotary_pos_embedding.get_pos_emb_on_this_cp_rank',
                 get_pos_emb_on_this_cp_rank)
             
-            # gdn feature
-            from mindspeed.core.ssm.gated_delta_net import GatedDeltaNet
-            patch_manager.register_patch('megatron.core.ssm.gated_delta_net.GatedDeltaNet', GatedDeltaNet)
+        # gdn feature
+        from mindspeed.core.ssm.gated_delta_net import GatedDeltaNet
+        patch_manager.register_patch('megatron.core.ssm.gated_delta_net.GatedDeltaNet', GatedDeltaNet)
