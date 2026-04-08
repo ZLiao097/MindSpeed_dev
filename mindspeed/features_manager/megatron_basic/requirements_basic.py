e65a3a8a955c12d07e79f0512bccae412e85ee97
chenjiao.angel <chenjiao.angel@bytedance.com>
2026-04-08 17:35:49 +0800

    activate triton gdn

--- mindspeed/features_manager/megatron_basic/requirements_basic.py
+++ mindspeed/features_manager/megatron_basic/requirements_basic.py
@@ -30,7 +30,8 @@ class RequirementsBasicFeature(MindSpeedFeature):
         from mindspeed.core.megatron_basic.requirements_basic import version_wrapper, dummy_compile
         from mindspeed.te.pytorch.module.layernorm import MindSpeedTELayernorm
         from mindspeed.ops.triton.l2norm import l2norm
-        from mindspeed.core.ssm.chunk_gated_delta_rule import torch_chunk_gated_delta_rule
+        # from mindspeed.core.ssm.chunk_gated_delta_rule import torch_chunk_gated_delta_rule
+        from mindspeed.core.ssm.chunk_gated_delta_rule import chunk_gated_delta_rule
         import torch_npu
         pm.register_patch('torch.cuda.nvtx.range_push', torch_npu.npu.mstx.range_start)
         pm.register_patch('torch.cuda.nvtx.range_pop', torch_npu.npu.mstx.range_end)
@@ -57,7 +58,8 @@ class RequirementsBasicFeature(MindSpeedFeature):
         pm.register_patch('transformer_engine.pytorch.ops.FusibleOperation', torch.nn.Module, create_dummy=True)
         pm.register_patch('flash_attn.flash_attn_interface.flash_attn_unpadded_func', create_dummy=True)
         pm.register_patch('fla.modules.l2norm.l2norm', l2norm, create_dummy=True)
-        pm.register_patch('fla.ops.gated_delta_rule.chunk_gated_delta_rule', torch_chunk_gated_delta_rule, create_dummy=True)
+        # pm.register_patch('fla.ops.gated_delta_rule.chunk_gated_delta_rule', torch_chunk_gated_delta_rule, create_dummy=True)
+        pm.register_patch('fla.ops.gated_delta_rule.chunk_gated_delta_rule', chunk_gated_delta_rule, create_dummy=True)
 
     def apex_adaptation(self, pm, args):
         from mindspeed.core.megatron_basic.requirements_basic import multi_tensor_l2norm, multi_tensor_scale, multi_tensor_applier
