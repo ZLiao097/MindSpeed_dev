55d0faf5158c13850e892a7b48d1d69eb1ba8f2c
chenjiao.angel <chenjiao.angel@bytedance.com>
2026-04-24 10:37:10 +0800

    fit torch 2.9.0 in gdn

--- mindspeed/core/ssm/gated_delta_net.py
+++ mindspeed/core/ssm/gated_delta_net.py
@@ -705,7 +705,10 @@ def get_parameter_local_cp(
     slices = [slice(None)] * param.dim()
     dim_size = param.size(dim=dim)
     slices[dim] = slice(cp_rank * dim_size // cp_size, (cp_rank + 1) * dim_size // cp_size)
-    param = param[slices]
+    if tuple(int(x) for x in torch.__version__.split('.')[:2]) >= (2, 9):
+        param = param[tuple(slices)]
+    else:
+        param = param[slices]
     return param
 
 
